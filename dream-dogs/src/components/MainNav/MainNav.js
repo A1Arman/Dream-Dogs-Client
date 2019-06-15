6ecf758412bc612b484379f38357cd99f3a20c3c
Arman Bhimani <Armanbhimani@gmail.com>
2019-06-15 18:27:38 -0500

    small fix

--- dream-dogs/src/components/MainNav/MainNav.js
+++ dream-dogs/src/components/MainNav/MainNav.js
@@ -10,7 +10,7 @@ function MainNav() {
             <Link to='/' className='nav-title'><img className='icon' src={logo} alt='dream dogs icon/logo'></img><span className='title'>Dream Dogs</span></Link>
             <Menu>
                 <Link to='/' id="home" className="menu-item">Home</Link><br />
-                <Link id="demo" className="menu-item" to='/posts'>Demo</Link><br />
+                <Link id="demo" className="menu-item" to='/posts'>Posts</Link><br />
                 <Link id="signup" className="menu-item" to='/signup'>Sign Up</Link><br />
                 <Link id="login" className="menu-item" to='/login'>Log In</Link> 
             </Menu>
