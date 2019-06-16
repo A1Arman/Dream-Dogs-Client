9f555a8886fcea728bf28a01cf9cc8db1bb43639
Arman Bhimani <Armanbhimani@gmail.com>
2019-06-16 14:10:09 -0500

    Added Logout to main nav

--- dream-dogs/src/components/MainNav/MainNav.js
+++ dream-dogs/src/components/MainNav/MainNav.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { slide as Menu } from 'react-burger-menu';
 import './MainNav.css';
 import logo from '../../images/dog.svg';
-
+import TokenService from '../../services/token-service';
 
 function MainNav(props) {
     return (
@@ -13,7 +13,7 @@ function MainNav(props) {
                 <Link to='/' id="home" className="menu-item">Home</Link><br />
                 <Link id="demo" className="menu-item" to='/posts'>Posts</Link><br />
                 <Link id="signup" className="menu-item" to='/signup'>Sign Up</Link><br />
-                {/*Line 16-19 recieves logged in state to either render log in or log out link */}
+                {/*Line 16-19 checks to see if auth token is in browser to either render log in or log out link */}
                 {TokenService.hasAuthToken() ? 
                     <Link to='/' className="menu-item" onClick={props.handleLogout}>Log out</Link>
                     : <Link id="login" className="menu-item" to='/login'>Log In</Link>
